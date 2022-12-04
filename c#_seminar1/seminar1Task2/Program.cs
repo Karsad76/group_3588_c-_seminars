051b896ee4b44f90e6c1c7a76a0e74e607ef8d54
Sergey <skarpov@gmail.com>
2022-12-04 14:01:13 +0300

    Added: sem1Task2 solution

---
+++ c#_seminar1/seminar1Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// taking console input
+Console.Write("Введите число: ");
+string? num1Line = Console.ReadLine();
+Console.Write("Введите другое число: ");
+string? num2Line = Console.ReadLine();
+
+// null string check
+if (num1Line != null && num2Line != null)
+{
+    // parsing to int
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    // compare who is max
+    if (num1 > num2)
+    {
+        // output TRUE
+        Console.WriteLine(num1 + " больше чем " + num2);
+    }
+    else
+    {
+        // output FALSE
+        Console.WriteLine(num2 + " больше чем " + num1);
+    }
+}
