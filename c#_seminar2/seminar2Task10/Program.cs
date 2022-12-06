e15f4d25c7fe9a3f132e40d5427b787922fa7f4e
Sergey <skarpov@gmail.com>
2022-12-06 15:23:23 +0300

    Added: sem1Task10 solution

---
+++ c#_seminar2/seminar2Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число от 100 до 999: ");     // data entry prompt
+int numA = int.Parse(Console.ReadLine() ?? "0");    // console input parsing to int
+
+if (numA>99 & numA<1000)
+{
+    Console.WriteLine((numA%100-numA%10)/10);       // output second digit of input if TRUE
+}
+else
+{
+    Console.WriteLine("Вы ввели число не из диапазона 100-999. Попробуйте еще раз."); // output if FALSE
+}
