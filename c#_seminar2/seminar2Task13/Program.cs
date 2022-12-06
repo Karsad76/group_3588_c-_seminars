5f188a52997b4c64abf6b650ff84304d6c8b8912
Sergey <skarpov@gmail.com>
2022-12-06 16:29:01 +0300

    Added: sem1Task13 solution

---
+++ c#_seminar2/seminar2Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите трех- или более разрядное число: ");     // data entry prompt
+int numA = int.Parse(Console.ReadLine() ?? "0");                // console input parsing to int
+int countNumOfDigits = 10;                                      // counter number of digits init
+
+if (Math.Abs(numA) > 99)                                        // input range validation
+{
+    while (Math.Abs(numA) / countNumOfDigits >= 1)              // cycle to count the number of digits 
+    {
+        countNumOfDigits = countNumOfDigits * 10;               // counter+
+    }
+    // output the third digit of input number if input range correct
+    Console.WriteLine("Третья цифра введенного числа: " + (Math.Abs(numA) / (countNumOfDigits / 1000)) % 10); 
+}
+else
+{
+    // output message if input range incorrect
+    Console.WriteLine("Вы ввели число с меньшим кол-вом разрядов. Третьей цифры в нем нет. Попробуйте снова");
+}
