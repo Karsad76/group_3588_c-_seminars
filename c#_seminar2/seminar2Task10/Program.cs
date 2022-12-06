46558e92c70aa214e2717bb80c3732c3543ae17f
Sergey <skarpov@gmail.com>
2022-12-06 15:47:03 +0300

    Edited: sem1Task10 solution work with negative range of input added

--- c#_seminar2/seminar2Task10/Program.cs
+++ c#_seminar2/seminar2Task10/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Введите число от 100 до 999: ");     // data entry prompt
-int numA = int.Parse(Console.ReadLine() ?? "0");    // console input parsing to int
+Console.Write("Введите любое трехзначное число: ");     // data entry prompt
+int numA = int.Parse(Console.ReadLine() ?? "0");        // console input parsing to int
 
-if (numA>99 & numA<1000)
+if (Math.Abs(numA)>99 & Math.Abs(numA)<1000)            // input range validation
 {
-    Console.WriteLine((numA%100-numA%10)/10);       // output second digit of input if TRUE
+    Console.WriteLine(Math.Abs((numA%100-numA%10)/10)); // output second digit of input if TRUE
 }
 else
 {
-    Console.WriteLine("Вы ввели число не из диапазона 100-999. Попробуйте еще раз."); // output if FALSE
+    Console.WriteLine("Вы ввели не трехзначное число. Попробуйте еще раз."); // output if FALSE
 }
