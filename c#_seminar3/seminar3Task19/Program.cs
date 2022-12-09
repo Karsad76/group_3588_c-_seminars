dc81519e69a1f82dd34b2f3ce795e7e9b681bebe
Sergey <skarpov@gmail.com>
2022-12-09 19:40:45 +0300

    Added: sem3 Tasks 19, 21, 23 solution (Homework)

---
+++ c#_seminar3/seminar3Task19/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int ReadData(string msg) // input data method
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int quat = ReadData("Введите пятизначное число: ");
+
+var palindromHashDict = new Dictionary<int, int>();
+
+void FillDictionary()   // fill dictionary with palindrom hashes method
+{
+    for (int i = 10; i < 100; i++)
+    {
+        int palindromHash = (i*100+((i % 10)*10 + i / 10));
+        palindromHashDict.Add(i, palindromHash);
+    }
+}
+
+void PrintDictionary()  // print dictionary method (for test only)
+{
+    for (int i = 10; i < 100; i++)
+    {
+        Console.WriteLine(palindromHashDict[i]);
+    }
+}
+
+FillDictionary();
+//PrintDictionary();
+
+if (palindromHashDict.ContainsValue((quat/1000)*100+quat%100) == true)  // check input hash via dictionary
+{
+    Console.WriteLine ("Введенное число является палиндромом");
+}
+else
+{
+    Console.WriteLine ("Введенное число не является палиндромом");
+}
---
+++ c#_seminar3/seminar3Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadData(string msg)    // input data method
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int x1 = ReadData("Введите координату Х точки А: ");
+int y1 = ReadData("Введите координату Y точки А: ");
+int z1 = ReadData("Введите координату Z точки А: ");
+int x2 = ReadData("Введите координату Х точки Б: ");
+int y2 = ReadData("Введите координату Y точки Б: ");
+int z2 = ReadData("Введите координату Z точки Б: ");
+
+double LenghtCalc(int x1, int y1, int x2, int y2, int z1, int z2)   // lenght calculate method
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+    return result;
+}
+
+void PrintData(string msg, double result)   // print data method
+{
+    Console.WriteLine(msg + result);
+}
+
+PrintData("Расстояние между точками: ", LenghtCalc(x1, y1, x2, y2, z1, z2));
