e684640e5cd4d36fcf9dc25a4abce46e75f5887e
Sergey <skarpov@gmail.com>
2022-12-22 18:47:51 +0300

    Added: sem6 tasks 41*, 43* solutions (HomeWork)

---
+++ c#_seminar6/seminar6Task41/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// (*) Пользователь вводит с клавиатуры любую последовательность символов в кол-ве М. Программа должна распознать
+// все положительные числа в последовательности.
+
+using System;
+using System.Text.RegularExpressions;
+
+int qtyOfTaps = ReadData("Укажите количество символов для ввода: ");    // data input prompt
+string tapsSequence = ReadStringLine("Введите любые символы с клавиатуры: ");   // data input prompt
+TakePositiveNumsFromInputString(tapsSequence);  // find numbers via Regex method
+
+// METHODS
+int ReadData(string msg)    // input data method
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+string ReadStringLine(string txt)   // input string method
+{
+    Console.Write(txt);
+    string s = String.Empty;
+    for (int i = 0; i < qtyOfTaps; i++)
+    {
+        var key = Console.ReadKey(false); //параметр true указан для того, чтобы после нажатия клавиш ничего не появлялось в консоли
+        s = s + String.Format(key.KeyChar.ToString());// + ","; 
+    }
+    Console.WriteLine();
+    return s;
+}
+
+void TakePositiveNumsFromInputString(string str)    // Regex method for take all positive numbers from string
+{
+    Regex regex = new Regex(@"(?<!-\d*)\d+");   // Regex pattern init
+    MatchCollection matches = regex.Matches(str);  
+    int posNums = 0;
+    if (matches.Count > 0) 
+    {
+        Console.Write("Введенные положительные числа: ");
+
+        foreach (Match match in matches)    // output finded numbers and counting of that
+        {
+            Console.Write($"[{match.Value}] ");
+            posNums = posNums + 1;
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Всего положительных чисел: {posNums}");
+    }
+    else
+    {
+        Console.WriteLine("Чисел не найдено");
+    }
+}
---
+++ c#_seminar6/seminar6Task43/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// (*) Найдите площадь треугольника образованного пересечением 3 прямых (y = k3 * x + b3)
+
+string newArg = ReadFromConsole("Введите значения К и В для трех уравнений в формате К(1,2,3), B(1,2,3): ").ToString();
+double k1, k2, k3, b1, b2, b3;  // global variables for equation arguments init
+double x1, x2, x3, y1, y2, y3;  // global variables for points coordinates of intersection of lines init
+
+ParsingStringToDouble(newArg);  // parsing equation arguments from input string
+PointIntersectionCoordsFind(k1, b1, k2, b2, k3, b3);    // finding points coordinates of intersection of lines
+
+double sideA = LenghtOfSegmentCalc(x1, y1, x2, y2);     // calculating the length of triangle sides
+double sideB = LenghtOfSegmentCalc(x2, y2, x3, y3);
+double sideC = LenghtOfSegmentCalc(x3, y3, x1, y1);
+
+PrintData($"Координаты точек пересечения прямых: ({x1.ToString("0.000")}, {y1.ToString("0.000")}), ({x2.ToString("0.000")}, {y2.ToString("0.000")}), ({x3.ToString("0.000")}, {y3.ToString("0.000")})");
+PrintData($"Длины сторон треугольника: сторона A = {sideA.ToString("#.000")}; сторона B = {sideB.ToString("#.000")}, сторона C = {sideC.ToString("#.000")}");
+PrintData($"Площадь треугольника S = {TriangleAreaCalc(sideA, sideB, sideC).ToString("#.000")}");
+
+string ReadFromConsole(string msg)    // input data method
+{
+    Console.Write(msg);
+    string inputString = Console.ReadLine() ?? "";
+    return inputString;
+}
+
+void ParsingStringToDouble(string text) // parsing numbers from input string to double
+{
+    text = text.Replace("K", "").Replace("B", "").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("К", "").Replace("В", "");
+    string[] digits = text.Split(",");
+
+    k1 = double.Parse(digits[0]);
+    k2 = double.Parse(digits[1]);
+    k3 = double.Parse(digits[2]);
+    b1 = double.Parse(digits[3]);
+    b2 = double.Parse(digits[4]);
+    b3 = double.Parse(digits[5]);
+}
+
+void PrintData(string msg)   // print data method
+{
+    Console.WriteLine(msg);
+}
+
+void PointIntersectionCoordsFind(double argK1, double argB1, double argK2, double argB2, double argK3, double argB3) // method for finding points coordinates of intersection of lines
+{
+    x1 = (argB2 - argB1) / (argK1 - argK2);
+    y1 = (argK1 * argB2 - argK2 * argB1) / (argK1 - argK2);
+    x2 = (argB3 - argB2) / (argK2 - argK3);
+    y2 = (argK2 * argB3 - argK3 * argB2) / (argK2 - argK3);
+    x3 = (argB1 - argB3) / (argK3 - argK1);
+    y3 = (argK3 * argB1 - argK1 * argB3) / (argK3 - argK1);
+}
+
+double LenghtOfSegmentCalc(double coordX1, double coordY1, double coordX2, double coordY2)     // method for calculating the length of a segment
+{
+    double result = Math.Sqrt(Math.Pow((coordX1 - coordX2), 2) + Math.Pow((coordY1 - coordY2), 2));
+    return result;
+}
+
+double TriangleAreaCalc(double a, double b, double c)     // method for calculating the area of a triangle
+{
+    double squareT = Math.Sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+    return squareT;
+}
